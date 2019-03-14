5ad7133760b8c379d722c79fe9374c15a8611307
moti <moti@elector.co.il>
2019-03-14 17:09:28 +0200

    this is the last update

--- .idea/src/js.js
+++ .idea/src/js.js
@@ -17,18 +17,31 @@ function changeImage() {
                                   }
         }
 
+ var today = new Date ( );//today a date object
+
+ var days = new Array ( );
+ days[days.length] = "יום ראשון";//in index 0
+ days[days.length] = "יום שני";//in index 1 becouse 0 place isnt empty
+ days[days.length] = "יום שלישי";
+ days[days.length] = "יום רביעי";
+ days[days.length] = "יום חמישי";
+ days[days.length] = "יום שישי";
+ days[days.length] = "יום שבת";
+ var day=today.getDay();
+ var month=today.getMonth();
+ var year=today.getFullYear();
+ document.getElementById("date").innerHTML =document.write (days[day]  + day + "/" + month + "/" + year );
 
 
+ /*jQuery(document).ready(function($){
 
-/*jQuery(document).ready(function($){
-
-$('.black-button').on({
-     'click': function(){
-         $('#change-image').attr('src','styles/images/exit-button.png');
-     }
- });
+ $('.black-button').on({
+      'click': function(){
+          $('#change-image').attr('src','styles/images/exit-button.png');
+      }
+  });
 
 
- });*/
+  });*/
 
 
