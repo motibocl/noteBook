d589c57ff6044aef682c12460e310c3fb4967aef
moti <moti@elector.co.il>
2019-03-17 15:50:41 +0200

    this is the last update with js

--- .idea/src/js.js
+++ .idea/src/js.js
@@ -5,33 +5,48 @@
  });
 */
 
-function changeImage() {
-            if (document.getElementById("enterBtn").src == "http://localhost:63342/noteBook/.idea/src/styles/images/enter-button.png")
-            {
-
-                document.getElementById("enterBtn").src = "styles/images/exit-button.png";
-
-            } else if (document.getElementById("enterBtn").src == "http://localhost:63342/noteBook/.idea/src/styles/images/exit-button.png")
-                                  {
-                                      document.getElementById("enterBtn").src = "styles/images/enter-button.png";
-                                  }
-        }
-
  var today = new Date ( );//today a date object
 
  var days = new Array ( );
- days[days.length] = "יום ראשון";//in index 0
- days[days.length] = "יום שני";//in index 1 becouse 0 place isnt empty
- days[days.length] = "יום שלישי";
- days[days.length] = "יום רביעי";
- days[days.length] = "יום חמישי";
- days[days.length] = "יום שישי";
- days[days.length] = "יום שבת";
- var day=today.getDay();
- var month=today.getMonth();
- var year=today.getFullYear();
- document.getElementById("date").innerHTML =document.write (days[day]  + day + "/" + month + "/" + year );
+ days[0] = "יום ראשון,";//in index 0
+ days[1] = "יום שני,";//in index 1 becouse 0 place isnt empty
+ days[2] = "יום שלישי,";
+ days[3] = "יום רביעי,";
+ days[4] = "יום חמישי,";
+ days[5] = "יום שישי,";
+ days[6] = "יום שבת,";
+ document.getElementById("date").innerHTML =days[today.getDay()]+"<br />"+today.getDate()+"/"+(today.getMonth()+1)+"/"+today.getFullYear();
 
+
+ function changeImage() {
+     if( typeof changeImage.minutes == 'undefined'  ) {
+         changeImage.minutes = 0;
+     }
+     if( typeof changeImage.hours == 'undefined'  ) {
+         changeImage.hours = 0;
+     }
+     if( typeof changeImage.showTime == 'undefined'  ) {
+         changeImage.showTime = 0;
+     }
+     if (document.getElementById("enterBtn").src == "http://localhost:63342/noteBook/.idea/src/styles/images/enter-button2.png")
+     {
+         var time=new Date();
+         changeImage.minutes=time.getMinutes();
+         changeImage.hours=time.getHours();
+         document.getElementById("enterBtn").src = "styles/images/exit-button.png";
+
+     } else if (document.getElementById("enterBtn").src == "http://localhost:63342/noteBook/.idea/src/styles/images/exit-button.png")
+     {
+         var time2=new Date();
+         var minutes1=time2.getMinutes();
+         var hours1=time2.getHours();
+         //  changeImage.minutes=(minutes1-changeImage.minutes;
+         var min=(hours1*60+minutes1)-(changeImage.hours*60+changeImage.minutes);
+         changeImage.showTime+=min;
+         document.getElementById("workTime").innerHTML= "כמה זמן עבדתי:"+(changeImage.showTime/60).toString().split(".")[0]+"hr"+":" +(changeImage.showTime%60)+"min" ;
+         document.getElementById("enterBtn").src = "styles/images/enter-button2.png";
+     }
+ }
 
  /*jQuery(document).ready(function($){
 
